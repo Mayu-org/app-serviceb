53f633ba384875aca0948a00e8268ca6ec3668c6
Mayu <Mayu@DESKTOP-GRFN1CA>
2024-07-22 23:17:45 +0100

    added opentel support

--- serviceb/src/main/java/com/example/serviceb/OpenTelemetryConfig.java
+++ serviceb/src/main/java/com/example/serviceb/OpenTelemetryConfig.java
@@ -1,25 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.example.serviceb;
 
-/**
- *
- * @author Mayu
- */
-
-
-import io.opentelemetry.api.OpenTelemetry;
-import io.opentelemetry.api.common.Attributes;
-import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
-import io.opentelemetry.context.propagation.ContextPropagators;
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
-import io.opentelemetry.sdk.resources.Resource;
 import io.opentelemetry.sdk.trace.SdkTracerProvider;
 import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
-import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -27,18 +13,22 @@ import org.springframework.context.annotation.Configuration;
 public class OpenTelemetryConfig {
 
     @Bean
-    public OpenTelemetry openTelemetry() {
-        Resource resource = Resource.getDefault()
-                .merge(Resource.create(Attributes.of(ResourceAttributes.SERVICE_NAME, "serviceb"))); // or "service-b"
-
-        SdkTracerProvider sdkTracerProvider = SdkTracerProvider.builder()
-                .addSpanProcessor(BatchSpanProcessor.builder(OtlpGrpcSpanExporter.builder().build()).build())
-                .setResource(resource)
+    public OpenTelemetrySdk openTelemetrySdk() {
+        OtlpGrpcSpanExporter spanExporter = OtlpGrpcSpanExporter.builder()
+                .setEndpoint("http://otel-collector.monitoring.svc.cluster.local:4317")
                 .build();
+        SdkTracerProvider tracerProvider = SdkTracerProvider.builder()
+                .addSpanProcessor(BatchSpanProcessor.builder(spanExporter).build())
+                .build();
+        OpenTelemetrySdk openTelemetrySdk = OpenTelemetrySdk.builder()
+                .setTracerProvider(tracerProvider)
+                .build();
+        GlobalOpenTelemetry.set(openTelemetrySdk);
+        return openTelemetrySdk;
+    }
 
-        return OpenTelemetrySdk.builder()
-                .setTracerProvider(sdkTracerProvider)
-                .setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()))
-                .buildAndRegisterGlobal();
+    @Bean
+    public Tracer tracer(OpenTelemetrySdk openTelemetrySdk) {
+        return openTelemetrySdk.getTracer("com.example.serviceb");
     }
 }
--- serviceb/src/main/java/com/example/serviceb/ServiceBApplication.java
+++ serviceb/src/main/java/com/example/serviceb/ServiceBApplication.java
@@ -1,37 +1,26 @@
 package com.example.serviceb;
 
+
+import io.opentelemetry.instrumentation.annotations.WithSpan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.opentelemetry.api.OpenTelemetry;
-import io.opentelemetry.api.trace.Span;
-import io.opentelemetry.api.trace.Tracer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ServiceBApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(ServiceBApplication.class, args);
     }
 }
 
-
-
 @RestController
 class ServiceBController {
-    private final Tracer tracer;
-
-    public ServiceBController(OpenTelemetry openTelemetry) {
-        this.tracer = openTelemetry.getTracer(ServiceBController.class.getName());
-    }
 
     @GetMapping("/info")
+    @WithSpan
     public String getInfo() {
-        Span span = tracer.spanBuilder("getInfo").startSpan();
-        try (var scope = span.makeCurrent()) {
-            return "Hello from Service B, this is a test! Making a change :)";
-        } finally {
-            span.end();
-        }
+        return "Hello from Service B";
     }
 }
