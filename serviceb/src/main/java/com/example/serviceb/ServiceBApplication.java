b718827a0938b7b00c2f9d8c12afe30e4c3708cf
Mayu <Mayu@DESKTOP-GRFN1CA>
2024-07-31 23:15:58 +0100

    added added log stmts using log4j

--- serviceb/src/main/java/com/example/serviceb/ServiceBApplication.java
+++ serviceb/src/main/java/com/example/serviceb/ServiceBApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @SpringBootApplication
 public class ServiceBApplication {
@@ -17,10 +21,15 @@ public class ServiceBApplication {
 
 @RestController
 class ServiceBController {
+    private static final Logger logger = LogManager.getLogger(ServiceBController.class);
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
 
     @GetMapping("/info")
     @WithSpan
     public String getInfo() {
+        String timestamp = LocalDateTime.now().format(formatter);
+        logger.debug("Sending response to Service A at : {}", timestamp);
+        logger.info("Sending info entry here while responding to Service a at {}",timestamp);
         return "Hello from Service B";
     }
 }
