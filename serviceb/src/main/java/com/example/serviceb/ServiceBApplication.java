6ad5e01e5c97348defd32ac3653cfa4c998443b3
Mayu-intellij <vicky.pardeshi@gmail.com>
2025-02-13 12:50:25 +0000

    remove otel

--- serviceb/src/main/java/com/example/serviceb/ServiceBApplication.java
+++ serviceb/src/main/java/com/example/serviceb/ServiceBApplication.java
@@ -1,5 +1,6 @@
 package com.example.serviceb;
 
+
 //import io.opentelemetry.instrumentation.annotations.WithSpan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
